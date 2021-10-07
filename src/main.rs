c846dda8930ac9384f3ce4bd3cd3693f6400bb72
craigag@protonmail.com <craigag@protonmail.com>
2021-10-07 23:24:59 +0100

    fixed space/tab mixing.

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,116 @@
 
 struct CPU {
-    pc: u8,
-    a: u8,
-    b: u8,
-    c: u8,
-    alu: u8,
-    mar: u8,
-    mdr: u8,
-    ir: u8,
-    bus: u8,
-    sp: u8,
-    eeprom: [u16; 8192],
-    ram: [u8; 256],
-    rom: [u8; 256],
-    halt: u8, 
-    flags: u8,
+	pc: u8,
+	a: u8,
+	b: u8,
+	c: u8,
+	alu: u8,
+	mar: u8,
+	mdr: u8,
+	ir: u8,
+	bus: u8,
+	sp: u8,
+	eeprom: [u16; 8192],
+	ram: [u8; 256],
+	rom: [u8; 256],
+	halt: u8, 
+	flags: u8,
 }
 
 fn xor (a: u8, b: u8) -> u8 {
-    a ^ b
+	a ^ b
 }
 
 fn and (a: u8, b :u8) -> u8 {
-    a & b
+	a & b
 }
 
 fn or (a: u8, b: u8) -> u8 {
-    a | b
+	a | b
 }
 
 fn not (a: u8) -> u8 {
-    !a
+	!a
 }
 
 fn add (a: u8, mut b: u8, flags: &mut u8, subtract: u8) -> u8 {
-    let mut z: u8 = 0x00;
-    let mut carry: u8 = 0x00;
+	let mut z: u8 = 0x00;
+	let mut carry: u8 = 0x00;
 
-    if subtract & 0x01 == 0x01 {
-        b = add(not(b), 0x01, &mut 0x00, 0x00);
-    }
+	if subtract & 0x01 == 0x01 {
+		b = add(not(b), 0x01, &mut 0x00, 0x00);
+	}
 
-    for bit in 0..8 {
-        let out: u8 = xor(xor((a >> bit) & 0x01, (b >> bit) & 0x01), carry);
-        z |= out << bit;
-        carry = or(and((a >> bit) & 0x01, (b >> bit) & 0x01), and(xor((a >> bit) & 0x01, (b >> bit) & 0x01), carry));
-    }
+	for bit in 0..8 {
+		let out: u8 = xor(xor((a >> bit) & 0x01, (b >> bit) & 0x01), carry);
+		z |= out << bit;
+		carry = or(and((a >> bit) & 0x01, (b >> bit) & 0x01), and(xor((a >> bit) & 0x01, (b >> bit) & 0x01), carry));
+	}
 
-    *flags = *flags | (carry << 0);
-    if z == 0x00 {
-        *flags = *flags | (1 << 1);
-    } else {
-        *flags = *flags | (0 << 1);
-    }
-    z
+	*flags = *flags | (carry << 0);
+	if z == 0x00 {
+		*flags = *flags | (1 << 1);
+	} else {
+		*flags = *flags | (0 << 1);
+	}
+	z
 }
 
 fn get_address(_cpu: &mut CPU, inst: u8, t: u8) -> u16 {
-    let address: u16 = (t as u16) << 8 | (inst as u16) | (_cpu.flags as u16);
-    return address;
+	let address: u16 = (t as u16) << 8 | (inst as u16) | (_cpu.flags as u16);
+	return address;
 }
 
 fn load_code(_cpu: &mut CPU, code: [u8; 256]){
-    for i in 0..255 {
-        _cpu.rom[i] = code[i];
-    }
+	for i in 0..255 {
+		_cpu.rom[i] = code[i];
+	}
 }
 
 fn execute_micro_instruction(_cpu: &mut CPU, step: u8) {
-    if _cpu.ir == 0xff {
-        _cpu.halt = 0x00;
-        return;
-    }
+	if _cpu.ir == 0xff {
+		_cpu.halt = 0x00;
+		return;
+	}
 
-    match _cpu.eeprom[get_address(_cpu, _cpu.ir, step)  as usize] {
-        0x07C => { _cpu.bus = _cpu.pc; _cpu.mar = _cpu.bus; },
-        0x07D => { _cpu.bus = _cpu.pc; _cpu.mar = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
-        0x069 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.ir = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
-        0x06D => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.mar = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
-        0x05A => { _cpu.bus = _cpu.ram[_cpu.mar as usize]; _cpu.mdr = _cpu.bus; },
-        0x044 => { _cpu.bus = _cpu.mdr; _cpu.a = _cpu.bus; },
-        0x042 => { _cpu.bus = _cpu.mdr; _cpu.b = _cpu.bus; },
-        0x014 => { _cpu.alu = add(_cpu.a, _cpu.b, &mut _cpu.flags, 0x00); _cpu.bus = _cpu.alu; _cpu.a = _cpu.bus; },
-        0x314 => { _cpu.alu = add(_cpu.a, _cpu.b, &mut _cpu.flags, 0x01); _cpu.bus = _cpu.alu; _cpu.a = _cpu.bus; },
-        0x06E => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.pc = _cpu.bus; },
-        0x034 => { _cpu.bus = _cpu.a; _cpu.c = _cpu.bus; println!("{}", _cpu.c); },
-        0x065 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.a = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
-        0x063 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.b = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
-        0x0B1 => { _cpu.bus = _cpu.a; _cpu.ram[_cpu.mar as usize] = _cpu.bus; },
-        0x0B0 => { _cpu.bus = _cpu.b; _cpu.ram[_cpu.mar as usize] = _cpu.bus; },
-        0x2B0 => { _cpu.bus = _cpu.a; _cpu.ram[(_cpu.sp + 128) as usize] = _cpu.bus; _cpu.sp = _cpu.sp + 1; },
-        0xAE1 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.ram[(_cpu.sp + 128)  as usize] = _cpu.bus; _cpu.sp = _cpu.sp + 1; _cpu.pc = _cpu.pc + 1; },
-        0x454 => { _cpu.sp = _cpu.sp - 1; _cpu.bus = _cpu.ram[(_cpu.sp + 128) as usize]; _cpu.a = _cpu.bus; },
-        0x2A0 => { _cpu.bus = _cpu.b; _cpu.ram[(_cpu.sp + 128) as usize] = _cpu.bus; _cpu.sp = _cpu.sp + 1; },
-        0x452 => { _cpu.sp = _cpu.sp - 1; _cpu.bus = _cpu.ram[(_cpu.sp + 128) as usize]; _cpu.b = _cpu.bus; },
-        _ => return,
-    }
+	match _cpu.eeprom[get_address(_cpu, _cpu.ir, step)  as usize] {
+		0x07C => { _cpu.bus = _cpu.pc; _cpu.mar = _cpu.bus; },
+		0x07D => { _cpu.bus = _cpu.pc; _cpu.mar = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
+		0x069 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.ir = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
+		0x06D => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.mar = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
+		0x05A => { _cpu.bus = _cpu.ram[_cpu.mar as usize]; _cpu.mdr = _cpu.bus; },
+		0x044 => { _cpu.bus = _cpu.mdr; _cpu.a = _cpu.bus; },
+		0x042 => { _cpu.bus = _cpu.mdr; _cpu.b = _cpu.bus; },
+		0x014 => { _cpu.alu = add(_cpu.a, _cpu.b, &mut _cpu.flags, 0x00); _cpu.bus = _cpu.alu; _cpu.a = _cpu.bus; },
+		0x314 => { _cpu.alu = add(_cpu.a, _cpu.b, &mut _cpu.flags, 0x01); _cpu.bus = _cpu.alu; _cpu.a = _cpu.bus; },
+		0x06E => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.pc = _cpu.bus; },
+		0x034 => { _cpu.bus = _cpu.a; _cpu.c = _cpu.bus; println!("{}", _cpu.c); },
+		0x065 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.a = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
+		0x063 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.b = _cpu.bus; _cpu.pc = _cpu.pc + 1; },
+		0x0B1 => { _cpu.bus = _cpu.a; _cpu.ram[_cpu.mar as usize] = _cpu.bus; },
+		0x0B0 => { _cpu.bus = _cpu.b; _cpu.ram[_cpu.mar as usize] = _cpu.bus; },
+		0x2B0 => { _cpu.bus = _cpu.a; _cpu.ram[(_cpu.sp + 128) as usize] = _cpu.bus; _cpu.sp = _cpu.sp + 1; },
+		0xAE1 => { _cpu.bus = _cpu.rom[_cpu.mar as usize]; _cpu.ram[(_cpu.sp + 128)  as usize] = _cpu.bus; _cpu.sp = _cpu.sp + 1; _cpu.pc = _cpu.pc + 1; },
+		0x454 => { _cpu.sp = _cpu.sp - 1; _cpu.bus = _cpu.ram[(_cpu.sp + 128) as usize]; _cpu.a = _cpu.bus; },
+		0x2A0 => { _cpu.bus = _cpu.b; _cpu.ram[(_cpu.sp + 128) as usize] = _cpu.bus; _cpu.sp = _cpu.sp + 1; },
+		0x452 => { _cpu.sp = _cpu.sp - 1; _cpu.bus = _cpu.ram[(_cpu.sp + 128) as usize]; _cpu.b = _cpu.bus; },
+		_ => return,
+	}
 }
 
 fn execute_program(_cpu: &mut CPU) {
-    loop {
-        for i in 0..8 {
-            execute_micro_instruction(_cpu, i as u8);
-        }
-        if _cpu.halt == 0x00 {
-            break;
-        }
-    }
+	loop {
+		for i in 0..8 {
+			execute_micro_instruction(_cpu, i as u8);
+		}
+		if _cpu.halt == 0x00 {
+			break;
+		}
+	}
 }
 
 fn load_eeprom(_cpu: &mut CPU) {
-    for i in 0..255 {
+	for i in 0..255 {
 		// any flags state
 		for j in 0..4 {
 			_cpu.eeprom[(j << 11) | i] = 0x07C; // (PC out, MAR in) for all instructions at microstep 0 --> X X 000 XXXX XXXX
@@ -310,39 +310,39 @@ fn load_eeprom(_cpu: &mut CPU) {
 }
 
 fn create_cpu() -> CPU {
-    let mut _cpu = CPU {
-        pc: 0,
-        a: 0,
-        b: 0,
-        c: 0,
-        alu: 0,
-        mar: 0,
-        mdr: 0,
-        ir: 0,
-        bus: 0,
-        sp: 0x80,
-        eeprom: [0; 8192],
-        ram: [0; 256],
-        rom: [0; 256],
-        halt: 0x01,
-        flags: 0,
-    };
-    load_eeprom(&mut _cpu);
-    return _cpu;
+	let mut _cpu = CPU {
+		pc: 0,
+		a: 0,
+		b: 0,
+		c: 0,
+		alu: 0,
+		mar: 0,
+		mdr: 0,
+		ir: 0,
+		bus: 0,
+		sp: 0x80,
+		eeprom: [0; 8192],
+		ram: [0; 256],
+		rom: [0; 256],
+		halt: 0x01,
+		flags: 0,
+	};
+	load_eeprom(&mut _cpu);
+	return _cpu;
 }
 
 fn main() {
  
-    let mut _cpu = create_cpu();
+	let mut _cpu = create_cpu();
 
 	// set up variables
-    _cpu.rom[0] = 0x04; 	//	load A immediate
-    _cpu.rom[1] = 0x00; 	//	value 0
+	_cpu.rom[0] = 0x04; 	//	load A immediate
+	_cpu.rom[1] = 0x00; 	//	value 0
 	_cpu.rom[2] = 0x80;		//	output first term 
-    _cpu.rom[3] = 0x06; 	//	store A
-    _cpu.rom[4] = 0x00; 	//	memory location 0x00 - 'a'
-    _cpu.rom[5] = 0x06;		//	store A
-    _cpu.rom[6] = 0x02;		//	memory location 0x02 - 'b'
+	_cpu.rom[3] = 0x06; 	//	store A
+	_cpu.rom[4] = 0x00; 	//	memory location 0x00 - 'a'
+	_cpu.rom[5] = 0x06;		//	store A
+	_cpu.rom[6] = 0x02;		//	memory location 0x02 - 'b'
 	_cpu.rom[7] = 0x04;		//	load A immediate
 	_cpu.rom[8] = 0x01;		// 	value 1
 	_cpu.rom[9] = 0x80;		//	output second term
@@ -352,7 +352,7 @@ fn main() {
 	_cpu.rom[13] = 0x0a;	//	value 10 - number of iterations
 	_cpu.rom[14] = 0x06;	//	store A
 	_cpu.rom[15] = 0x03;	//	memory location 0x03 - 'n'
-    
+	
 	// add terms
 	_cpu.rom[16] = 0x01; 	// load A from memory
 	_cpu.rom[17] = 0x00;	// memory location 0x00 'a'
@@ -384,5 +384,5 @@ fn main() {
 	// end program
 	_cpu.rom[36] = 0xFF;
 
-    execute_program(&mut _cpu);
+	execute_program(&mut _cpu);
 }
